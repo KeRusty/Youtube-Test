a9ab8e2bcb08ad33366cb586b886f7ef10aeebba
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-02-22 18:29:50 +0400

    initial styling done

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,30 +1,35 @@
 import React, { ReactElement, useState } from 'react';
 import { useNavigate } from "react-router-dom";
+import './styles.scss'
 
 function Home(): ReactElement | null {
     const [inputUrl, setInputUrl] = useState<string>("");
     const navigate = useNavigate();
 
     const handleSave = () => {
-        //   setVideoUrl(inputUrl);
-        console.log(inputUrl)
-        localStorage.setItem("videoUrl", inputUrl);
         localStorage.setItem("videoPosition", "0");
-        navigate("/video");
+        navigate('/video', { state: { videoUrl: inputUrl } });
     };
 
     return (
-        <div className="p-4 flex flex-col items-center">
-            <input
-                type="text"
-                placeholder="YouTube Video URL"
-                className="p-2 border rounded w-96"
-                value={inputUrl}
-                onChange={(e) => setInputUrl(e.target.value)}
-            />
-            <button className="mt-2 p-2 bg-blue-500 text-white rounded" onClick={handleSave}>
-                Save
-            </button>
+        <div className="home-container">
+            <div className='textbox-container'>
+                <input
+                    type="text"
+                    placeholder="YouTube Video URL"
+                    className="text-box"
+                    value={inputUrl}
+                    onChange={(e) => setInputUrl(e.target.value)}
+                />
+            </div>
+
+            <div className=''>
+
+                <button className="watch-button" onClick={handleSave}>
+                    Watch Video
+                </button>
+            </div>
+
         </div>
     );
 }
