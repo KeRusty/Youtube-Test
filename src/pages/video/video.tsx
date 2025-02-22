546edb79a1ce4a9afb58981023a5205a6398ff91
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-02-22 17:47:54 +0400

    created basic pages

--- src/pages/video/video.tsx
+++ src/pages/video/video.tsx
@@ -1,9 +1,27 @@
-import * as React from 'react';
+import React, { ReactElement, useState } from 'react';
+import ReactPlayer from "react-player";
+import { useNavigate } from "react-router-dom";
 
-function Video() {
+function Video(): ReactElement | null {
+    const navigate = useNavigate();
+    const [position, setPosition] = useState<number>(parseFloat(localStorage.getItem("videoPosition") || "0"));
+
+    const handleProgress = (state: { playedSeconds: number }) => {
+        localStorage.setItem("videoPosition", state.playedSeconds.toString());
+    };
     return (
-        <div>
-            VIDEO
+        <div className="p-4 flex flex-col items-center">
+            <ReactPlayer
+                url={'https://www.youtube.com/watch?v=63EAJJakvEU&ab_channel=IliaTS'}
+                playing={false}
+                controls
+                onProgress={handleProgress}
+                progressInterval={1000}
+                width="80%"
+                height="60vh"
+            />
+            <button className="mt-2 p-2 bg-red-500 text-white rounded" onClick={() => navigate("/")}>Edit</button>
+            <button className="mt-2 p-2 bg-green-500 text-white rounded" onClick={() => navigate("/gif")}>Go to GIF</button>
         </div>
     )
 }
