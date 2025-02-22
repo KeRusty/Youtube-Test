2341e5640f7d6360038c9cf78c0189ab324185d3
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-02-22 19:44:46 +0400

    gif page created

--- src/pages/gifs/gif.tsx
+++ src/pages/gifs/gif.tsx
@@ -1,12 +1,16 @@
 import React, { ReactElement } from 'react';
 import { useNavigate } from "react-router-dom";
+import './styles.scss'
 
 function Gif(): ReactElement | null {
     const navigate = useNavigate();
     return (
-        <div className="p-4 flex flex-col items-center">
-            <img src="https://media.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif" alt="Funny GIF" className="w-80 h-80" />
-            <button className="mt-2 p-2 bg-yellow-500 text-white rounded" onClick={() => navigate("/video")}>Back</button>
+        <div className="gif-container">
+            <div className='gif-player-container'>
+                <img src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2FsNXFpeGliOGl5anQwcDJvazl0Y25paW90dnRvZDZ5dDJjZjJhZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XreQmk7ETCak0/giphy.gif" alt="Funny GIF" className="w-80 h-80" />
+            </div>
+
+            <button className="watch-button" onClick={() => navigate("/video")}>Back</button>
         </div>
     )
 }
