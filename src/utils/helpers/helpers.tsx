b8131376d082c8d99ed2a99218f1d1fdff244a27
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-02-22 19:44:54 +0400

    added helpers

--- src/utils/helpers/helpers.tsx
+++ src/utils/helpers/helpers.tsx
@@ -2,3 +2,7 @@ export const isYouTubeLink = (url: string): boolean => {
     const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[a-zA-Z0-9_-]+(&.*)?$/;
     return youtubeRegex.test(url);
 };
+
+export const handleProgress = (state: { playedSeconds: number }) => {
+    localStorage.setItem("videoPosition", state.playedSeconds.toString());
+};
