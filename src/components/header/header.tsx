bc041620d99ae080e5e36415a701a68692f03bf0
Chathu Abeywickrama <chathu.abey93@gmail.com>
2025-02-22 18:40:59 +0400

    added home nav

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './styles.scss'
 
 function Header() {
+    const navigate = useNavigate();
     return (
-        <div className='header-container'>
+        <div className='header-container' onClick={() => navigate('/')}>
             <div className='header-text-container'>
                 <h2 className='header-text'>YOUTUBE PLAYER</h2>
             </div>
